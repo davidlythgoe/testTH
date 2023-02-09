324e7996e2074ee076562275a0608158c3dab2c6
davidlythgoe <79723570+davidlythgoe@users.noreply.github.com>
2023-02-09 09:13:40 +0000

    asd

---
+++ src/routes/+layout.js
@@ -0,0 +1,4 @@
+// if you want to generate a static html file
+// for your page.
+// Documentation: https://kit.svelte.dev/docs/page-options#prerender
+export const prerender = true;
---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+		paths:{
+		base:'/testTH'
+		}   
+    }),
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+};
+
+export default config;
